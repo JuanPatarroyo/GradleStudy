a2c5b24654fde71506ef9e7247338750eda7ea61
Juan Patarroyo Ardila <juanestebanpatarroyo@gmail.com>
2022-12-27 23:52:44 -0500

    Usign gradle with IntelliJ idea, eclipse and netbeans. And repositories implementation

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
+import org.lwjgl.system.windows.DISPLAY_DEVICE;
+
 public class HelloWorld {
     public static void main(String[] args){
+        DISPLAY_DEVICE.create();
         System.out.println("Testing HelloWorld by Java");
     }
 }
